497ef690fe579c9fb8a30933bbf4b53b3db25439
Hamilton97 <ryangilberthamilton@gmail.com>
2024-04-23 11:48:21 -0400

    add opca calc and example of how to use

--- src/geeopca/opca.py
+++ src/geeopca/opca.py
@@ -1,24 +1,38 @@
+from typing import Any
 import ee
 
 
-def pca(image: ee.Image, bands: list[str]) -> ee.Image:
-    pca_bands = [f"pc{_}" for _ in range(1, len(bands) + 1)]
-    array_image = image.select(bands).toArray()
+class Opca:
+    def __init__(self, time_series: list[ee.Image]) -> None:
+        self.pca = [self.compute_pca(_) for _ in time_series]
 
-    covariance = array_image.reduceRegion(
-        **{"reducer": ee.Reducer.covariance(), "maxPixels": 1e13}
-    )
+    def __getitem__(self, index) -> ee.Image:
+        return self.pca[index]
 
-    covariance_array = ee.Array(covariance.get("array"))
+    def compute_pca(self, image: ee.Image) -> ee.Image:
+        pc_names = self.get_names('pc_', ee.List.sequence(1, image.bandNames().size()))
+        array_image = image.toArray()
+        
+        covariance = array_image.reduceRegion(
+            **{"reducer": ee.Reducer.covariance(), "maxPixels": 1e13}
+        )
+        
+        covariance_array = ee.Array(covariance.get("array"))
 
-    eigens = covariance_array.eigen()
-    eigen_vec = eigens.slice(1, 1)
-    principal_compoents = ee.Image(eigen_vec).matrixMultiply(array_image.toArray(1))
+        eigens = covariance_array.eigen()
+        eigen_vec = eigens.slice(1, 1)
+        principal_compoents = ee.Image(eigen_vec).matrixMultiply(array_image.toArray(1))
+        return principal_compoents.arrayProject([0]).arrayFlatten([pc_names])
 
-    pc_image = principal_compoents.arrayProject([0]).arrayFlatten([pca_bands])
+    @staticmethod
+    def get_names(prefix: str, sequence: list[int] | ee.List):
+        return ee.List(sequence).map(lambda x: ee.String(prefix).cat(ee.Number(x).int()))
 
-    return pc_image
 
+def get_opca_min_max(pc_image: ee.Image) -> Any:
+    # Define a region of interest, which could be the whole image
+    roi = pc_image.geometry()
 
-def temporal_pca(data: list[ee.Image], bands: list[str]):
-    return [pca(image, bands) for image in data]
+    # Use the reducer to get the min and max
+    stats = pc_image.reduceRegion(reducer=ee.Reducer.minMax(), geometry=roi, bestEffort=True)
+    return stats
